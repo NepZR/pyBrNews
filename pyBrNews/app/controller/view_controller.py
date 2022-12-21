11dd62f9323683ab113d71a1802dcf82bd1c6785
Lucas Rodrigues <lucas.darlindo@gmail.com>
2022-12-21 02:42:49 -0300

    wip: Search into DB Backend and other enhancements

--- pyBrNews/app/controller/view_controller.py
+++ pyBrNews/app/controller/view_controller.py
@@ -1,6 +1,7 @@
-from typing import List
+from typing import List, Optional
 
 from pyBrNews.app.controller.database_controller import DatabaseController
+from pyBrNews.config.database import PyBrNewsDB
 from pyBrNews.news.g1 import G1News
 from pyBrNews.news.folha_sp import FolhaNews
 from pyBrNews.news.exame import ExameNews
@@ -11,7 +12,66 @@ class ViewController:
         self.g1 = G1News()
         self.folha = FolhaNews()
         self.exame = ExameNews()
-        self.db_controller = DatabaseController()
+        self.db_controller: Optional[DatabaseController] = None
+
+        self.set_db_backend()
+
+    def set_db_backend(self, db_backend: str = "mongodb") -> None:
+        self.db_controller = DatabaseController(db_backend=db_backend.lower())
+
+    def switch_db_backend(self) -> None:
+        if type(self.db_controller.database) is PyBrNewsDB:
+            self.set_db_backend(db_backend="elasticsearch")
+        else:
+            self.set_db_backend(db_backend="mongodb")
+
+    def retrieve_db_backend_info(self) -> str:
+        db_backend_info = "[DB Backend] "
+        if type(self.db_controller.database) is PyBrNewsDB:
+            db_backend_info += (
+                f"PyBrNewsDB (MongoDB), connected in "
+                f"{self.db_controller.database.host}:{self.db_controller.database.port}"
+            )
+        else:
+            db_backend_info += (
+                f"PyBrNewsES (ElasticSearch), connected in "
+                f"{self.db_controller.database.host}:{self.db_controller.database.port}"
+            )
+
+        return db_backend_info
+
+    def retrieve_db_switch_str(self) -> str:
+        if type(self.db_controller.database) is PyBrNewsDB:
+            db_switch_str = f"Switch to PyBrNewsES (ElasticSearch)"
+        else:
+            db_switch_str = f"Switch to PyBrNewsDB (MongoDB)"
+
+        return db_switch_str
+
+    def search_data(self, query_params: dict) -> List[List[str]]:
+        platforms = []
+        if query_params["search_query_platform_g1"]:
+            platforms.append("Portal G1")
+        if query_params["search_query_platform_folhasp"]:
+            platforms.append("Folha de São Paulo")
+        if query_params["search_query_platform_exame"]:
+            platforms.append("Exame")
+
+        search_query = (query_params["search_query_text"] + " and") if query_params["search_all_terms"] else (
+            query_params["search_query_text"]
+        )
+
+        search_init_final_date = (query_params["search_query_init_date"], query_params["search_query_final_date"])
+        search_limit = query_params["search_query_limit"]
+
+        query_response = [data for data in self.db_controller.database.read_data(
+            origin=platforms, search_query=search_query, init_final_year=search_init_final_date, limit=search_limit
+        )]
+
+        retrieved_data = [
+            [data["title"], data["platform"], data["_id"]] for data in query_response
+        ]
+        return retrieved_data
 
     def search_and_insert_news(self, search_params: dict) -> List[List[str]]:
         news_urls = {"g1": None, "folhasp": None, "exame": None}
@@ -21,23 +81,28 @@ class ViewController:
             news_urls["g1"] = self.g1.search_news(keywords=[search_term])
             for article_data in self.g1.parse_news(news_urls=news_urls["g1"], parse_body=True, save_html=False):
                 article_data["search_keyword"] = search_term
-                self.db_controller.insert_data(parsed_data=article_data)
+                self.db_controller.database.insert_data(parsed_data=article_data)
 
         if search_params["news_platform_folhasp"]:
-            news_urls["folhasp"] = self.folha.search_news(keywords=[search_term])
+            news_urls["folhasp"] = self.folha.search_news(keywords=[search_term], max_pages=1000)
             for article_data in self.folha.parse_news(news_urls=news_urls["folhasp"], parse_body=True, save_html=False):
                 article_data["search_keyword"] = search_term
-                self.db_controller.insert_data(parsed_data=article_data)
+                self.db_controller.database.insert_data(parsed_data=article_data)
 
         if search_params["news_platform_exame"]:
             news_urls["exame"] = self.exame.search_news(keywords=[search_term])
             for article_data in self.exame.parse_news(news_urls=news_urls["exame"], parse_body=True, save_html=False):
                 article_data["search_keyword"] = search_term
-                self.db_controller.insert_data(parsed_data=article_data)
+                self.db_controller.database.insert_data(parsed_data=article_data)
+
+        query_response = self.db_controller.database.collection.find(
+                {"search_keyword": search_term}
+            ).limit(30) if type(self.db_controller.database) is PyBrNewsDB else self.db_controller.database.db.search(
+            index=self.db_controller.database.index, body={"query": {"bool": {"must": [{"match": {
+                "search_keyword.keyword": search_term}}]}}}, size=30
+        )["hits"]["hits"]["_source"]
 
         retrieved_data = [
-            [data["title"], data["platform"]] for data in self.db_controller.collection.find(
-                {"search_keyword": search_term}
-            ).limit(30)
+            [data["title"], data["platform"]] for data in query_response
         ]
         return retrieved_data
--- pyBrNews/app/view/view.py
+++ pyBrNews/app/view/view.py
@@ -16,7 +16,7 @@ class PyBrNewsView(ViewController):
             element_justification="center",
             default_element_size=(400, 480),
             grab_anywhere=False,
-            size=(800, 650)
+            size=(800, 750)
         )
 
     def _user_layout(self) -> List[Any]:
@@ -47,7 +47,7 @@ class PyBrNewsView(ViewController):
                     )],
                     [self.ui_handler.Table(
                         values=[], headings=["Title", "Platform"], key="-OUTPUT_DATA-", justification="left",
-                        col_widths=[60, 20], expand_x=True, expand_y=True, auto_size_columns=False, num_rows=15
+                        col_widths=[60, 20], expand_x=True, expand_y=True, auto_size_columns=False, num_rows=20
                     )]
                 ]
             )]
@@ -55,44 +55,58 @@ class PyBrNewsView(ViewController):
 
         search_tab = [
             [self.ui_handler.Column(
-                key="-NEWS-ACTIONS-", expand_x=True,
+                key="-SEARCH-ACTIONS-", expand_x=True,
                 layout=[
                     [self.ui_handler.Text('Search Query: ', font=("Segoi UI", 11, "bold")),
-                     self.ui_handler.InputText('', key='search_query', expand_x=True)],
+                     self.ui_handler.InputText('', key='search_query_text'),
+                     ],
+                    [self.ui_handler.Checkbox(
+                        'Use \"AND\" Query', font=("Segoi UI", 11, "bold"), key="search_all_terms", expand_x=True
+                    ), self.ui_handler.Text('Query Limit', font=("Segoi UI", 11, "bold")),
+                    ],
                     [
                         self.ui_handler.Text('Search Platforms', font=("Segoi UI", 11, "bold"), expand_x=True),
-                        self.ui_handler.Checkbox('Portal G1', key='news_platform_g1'),
-                        self.ui_handler.Checkbox('Folha de São Paulo', key='news_platform_folhasp'),
-                        self.ui_handler.Checkbox('Exame', key='news_platform_exame')
-                    ],
-                    [
-                        self.ui_handler.Text(
-                            'Query Limit (0 = No limit)', font=("Segoi UI", 11, "bold")),
+                        self.ui_handler.Text(expand_x=True),
                         self.ui_handler.Combo(
-                            [i for i in range(0, 1100, 100)],
+                            [i for i in range(0, 1100, 100)], key="search_query_limit"
                         )
                     ],
                     [
-                        self.ui_handler.Text(
-                            'Query Limit (0 = No limit)', font=("Segoi UI", 11, "bold")),
-                        self.ui_handler.Combo(
-                            [i for i in range(0, 1100, 100)],
-                        )
+                        self.ui_handler.Checkbox('Portal G1', key='search_query_platform_g1'),
+                        self.ui_handler.Checkbox('Folha de São Paulo', key='search_query_platform_folhasp'),
+                        self.ui_handler.Checkbox('Exame', key='search_query_platform_exame'),
                     ],
                     [self.ui_handler.Col(
-                        [[self.ui_handler.Button(
-                            'Run Query', key="get_news", enable_events=True
-                        )]], pad=(0, 10), justification="right"
+                        [[
+                            self.ui_handler.Text(
+                                "Please, set the start date.", key="-QUERY_INIT_DT-"
+                            ),
+                            self.ui_handler.CalendarButton(
+                                "Set Start Date", key="search_query_init_date",
+                                format="%Y-%m-%d",
+                            ),
+                            self.ui_handler.VerticalSeparator(color="black", pad=(5, 0)),
+                            self.ui_handler.Text("Please, set the final date.", key="-QUERY_END_DT-"),
+                            self.ui_handler.CalendarButton(
+                                "Set Final Date", key="search_query_final_date",
+                                format="%Y-%m-%d"
+                            ),
+                            self.ui_handler.VerticalSeparator(color="black", pad=(5, 0)),
+                        ]], justification="left"),
+                        self.ui_handler.Col(
+                            [[self.ui_handler.Button(
+                                'Run Query', key="run_search_query", enable_events=True
+                            )]], pad=(0, 10), justification="right"
                     )],
 
                     [self.ui_handler.HorizontalSeparator(color="black", pad=(0, 0))],
                     [self.ui_handler.Text(
-                        f'Result Output (limited to 30)', font=("Segoi UI", 16, "bold"),
+                        f'Search Result', font=("Segoi UI", 16, "bold"),
                         border_width=1, pad=(5, 5), key="-DOUT_SEARCH"
                     )],
                     [self.ui_handler.Table(
-                        values=[], headings=["Title", "Platform"], key="-OUTPUT_DATA-", justification="left",
-                        col_widths=[60, 20], expand_x=True, expand_y=True, auto_size_columns=False, num_rows=15,
+                        values=[], headings=["Title", "Platform", "ID"], key="-RESULT_DATA-", justification="left",
+                        col_widths=[60, 20, 10], expand_x=True, expand_y=True, auto_size_columns=False, num_rows=15,
                         enable_click_events=False
                     )],
                     [self.ui_handler.Button(
@@ -126,16 +140,23 @@ class PyBrNewsView(ViewController):
             [self.ui_handler.TabGroup([[
                 self.ui_handler.Tab('Parse News', actions_tab),
                 self.ui_handler.Tab('Search Data', search_tab),
-                self.ui_handler.Tab('Manage Data (Inspection Tool)', inspect_tab),
+                self.ui_handler.Tab('Inspection Tool', inspect_tab),
             ]])
+            ],
+            [self.ui_handler.Text(
+               self.retrieve_db_backend_info(), expand_x=True, justification="left",
+               font=("Segoi UI", 12, "bold"), key="-DB_BACKEND_INFO-", pad=(0, 15)
+            ), self.ui_handler.Button(
+                button_text=self.retrieve_db_switch_str(), key="db_backend_set", enable_events=True
+            )
             ],
             [self.ui_handler.HorizontalSeparator(color="black", pad=(0, 10))],
             [self.ui_handler.Text(
-                '© 2022 Lucas Rodrigues\nBuild v0.1.2 (Dev Build)',
+                "© 2022 Lucas Rodrigues\nBuild v0.2.0-alpha1 (Branch: college/app-project)",
                 font=("Segoi UI", 11), expand_x=True, justification="center"
             )],
             [self.ui_handler.Text(
-                'pyBrNews Project, made with ❤️ by Lucas Rodrigues (@NepZR)',
+                "pyBrNews Project, made with ❤️ by Lucas Rodrigues (@NepZR)",
                 font=("Segoi UI", 9, "bold"), expand_x=True, justification="center"
             )],
             [self.ui_handler.HorizontalSeparator(color="black", pad=(0, 10))],
